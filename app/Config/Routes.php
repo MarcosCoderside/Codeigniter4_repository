8f72ec44cc9c84c631d5daa21bac70d0f1fb841e
MarcosCoderside <marcos_ushimata@yahoo.com.br>
2020-01-08 14:18:14 -0300

    branch principal

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -72,6 +72,9 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
+
+$routes->get('index', 'MainController::index');
+
 $routes->get('/', 'Home::index');
 
 /**
